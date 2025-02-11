fe778a6a430d886afaf805ead37a707f8b9e716e
MaryMachuma <machhmary173@gmail.com>
2025-02-11 17:13:22 +0300

    ipdb

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -3,5 +3,5 @@
 import ipdb
 
 def plus_two(num):
-    num + 2
+    num += 2 # Fix: update num with the result of adding 2
     return num
--- lib/ipdb_is_awesome.py
+++ lib/ipdb_is_awesome.py
@@ -3,7 +3,7 @@
 import ipdb
 
 def tracing_the_function():
-    inside_the_function = "We're inside the function"
+    inside_the_function = "We're inside the function!"
     print(inside_the_function)
     print("We're about to stop because of ipdb!")
     ipdb.set_trace()
